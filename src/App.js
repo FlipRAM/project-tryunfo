41041fe7adf648c642d813d99ed186188f712f4e
Felipe Rangel <feliperangel277@gmail.com>
2022-02-24 19:15:52 -0300

    Adiciona requisito 6 - cria a funcao de salvar

--- src/App.js
+++ src/App.js
@@ -17,11 +17,13 @@ class App extends React.Component {
       cardTrunfo: false,
       hasTrunfo: false,
       isSaveButtonDisabled: true,
+      savedCards: [],
     };
 
     this.onInputChange = this.onInputChange.bind(this);
     this.validateForm = this.validateForm.bind(this);
-    // this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
+    this.addNewCard = this.addNewCard.bind(this);
+    this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
   }
 
   onInputChange({ target }) {
@@ -30,6 +32,47 @@ class App extends React.Component {
     this.setState({
       [name]: value,
     }, () => this.validateForm());
+  }
+
+  onSaveButtonClick(event) {
+    event.preventDefault();
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.state;
+
+    const newCard = {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    };
+
+    this.addNewCard(newCard);
+
+    this.setState({
+      cardName: '',
+      cardDescription: '',
+      cardImage: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardRare: 'normal',
+    });
+  }
+
+  addNewCard(card) {
+    this.setState((prevState) => ({ savedCards: [...prevState.savedCards, card] }));
   }
 
   validateForm() {
@@ -81,7 +124,7 @@ class App extends React.Component {
         <h1>Tryunfo!</h1>
         <Form
           onInputChange={ this.onInputChange }
-          // onSaveButtonClick={ this.onSaveButtonClick }
+          onSaveButtonClick={ this.onSaveButtonClick }
           cardName={ cardName }
           cardDescription={ cardDescription }
           cardAttr1={ cardAttr1 }
